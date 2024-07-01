61a29e003b8755019c9ee01ae736f280cb6ab24d
kennie27 <nderuken@gmail.com>
2024-07-01 15:14:39 +0300

    button

---
+++ Javascript/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCRkwtRY0jOmdlidb6LZ49HpqzbRgliplI",
+  authDomain: "juakem-8310e.firebaseapp.com",
+  projectId: "juakem-8310e",
+  storageBucket: "juakem-8310e.appspot.com",
+  messagingSenderId: "1099251407701",
+  appId: "1:1099251407701:web:e8c12914d9cf6a34fc6ddc",
+  measurementId: "G-K3W0E5PEL8",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
